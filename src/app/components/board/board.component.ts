3ba73354b6d7ab9ccde34ef4c9fc6dc0bde6a347
YOUR_USERNAME <fayrouzamor456@gmail.com>
2020-04-03 14:24:43 +0200

    second commit

--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 
 
 @Component({
@@ -9,16 +9,25 @@ import { Component, OnInit } from '@angular/core';
 export class BoardComponent implements OnInit {
 
   private cells: string[] = [];
-  private turn = 'x';
+  private turn : string;
   private gameover = false;
-  private winner = null;
-
-  ngOnInit() {
-    for (let i = 0; i < 9; i++) {
-      this.cells[i] = null;
-    }
+  private winner : string;
+  private xIsNext: boolean;
+ 
+  get player() {
+    return this.xIsNext ? 'X' : 'O';
   }
 
+  constructor() {}
+
+  ngOnInit() {
+    this.newGame();
+  }
+ newGame() {
+    this.cells = Array(9).fill(null);
+    this.winner = null;
+    this.xIsNext = true;
+  }
   reset() {
     for (let i = 0; i < 9; i++) {
       this.cells[i] = null;
@@ -33,25 +42,44 @@ export class BoardComponent implements OnInit {
       if (this.cells[idx] === null) {
         this.cells[idx] = this.turn;
         this.checkWinner();
-        this.changeTurn();
+        this.changeTurn(idx);
       }
     }
   }
 
-  changeTurn() {
+  changeTurn(idx:number) {
     /* to Complete **/
-    if(!this.gameover){
-      this.turn='O';
-      this.gameover = false;
-      this.winner = 1;
+    if (!this.cells[idx]) {
+      this.cells.splice(idx, 1, this.player);
+      this.xIsNext = !this.xIsNext;
     }
-    else {
-      this.turn='X';
-    }
+
+    this.winner = this.checkWinner();
+    
   }
 
   checkWinner() {
     /* To complete **/
-   this.winner="";
-  }
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        this.cells[a] &&
+        this.cells[a] === this.cells[b] &&
+        this.cells[a] === this.cells[c]
+      ) {
+        return this.cells[a];
+      }
+    }
+    return this.winner;
+   }
 }
